3e2c8f8d7786a0c03c5c7370dcb34542fc2916ff
Jahnvi <jahnvi2205@gmail.com>
2025-08-19 22:03:45 +0530

    QUes1 Done

---
+++ Strings/Ques1.py
@@ -0,0 +1,10 @@
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+def reverseString(s: List[str]):
+        start= 0
+        end= len(s)-1
+        while start<= end:
+            s[start],s[end]= s[end],s[start]
+            start+= 1
+            end-= 1
