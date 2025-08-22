f98f472a66d5ef5210692eeb58ade0277316f328
Jahnvi <jahnvi2205@gmail.com>
2025-08-22 22:03:48 +0530

    Ques10 done

---
+++ Strings/Ques10.py
@@ -0,0 +1,12 @@
+# Input: "abc"
+# Output: '', 'c', 'b', 'bc', 'a', 'ac', 'ab', 'abc'
+
+def print_all(input,output):
+    if len(input)==0:
+        print(f"'{output}'", end=", ")
+        return
+
+    print_all(input[1:],output)
+    print_all(input[1:],output+input[0])
+    
+
