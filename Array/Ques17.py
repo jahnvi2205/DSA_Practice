bb4c94caac1dea44ded30bb64919f93c4383814e
Jahnvi <jahnvi2205@gmail.com>
2025-08-25 22:47:24 +0530

    ques17 done

---
+++ Array/Ques17.py
@@ -0,0 +1,10 @@
+def maxProfit(self, prices: List[int]) -> int:
+        profit=0
+        minp=prices[0]
+        for i in range(1,len(prices)):
+            if prices[i]<minp:
+                minp=prices[i]
+            calc= prices[i]-minp
+            profit=max(profit,calc)
+
+        return profit
