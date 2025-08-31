461550b7b1c51af16a1cc0c49a8e3f2c1af4e6d1
Jahnvi <jahnvi2205@gmail.com>
2025-08-31 23:54:26 +0530

    QUes27 Done

---
+++ Array/Ques27.py
@@ -0,0 +1,8 @@
+from collections import Counter
+ def isSubset(self, a, b):
+    count_a = Counter(a)
+    count_b = Counter(b)
+    for num in count_b:
+        if count_a[num] < count_b[num]:
+            return False
+    return True
