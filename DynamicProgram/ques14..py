545d548aaeb3c73ee0a0eebecf2a24bdb53003db
Jahnvi <jahnvi2205@gmail.com>
2025-09-24 00:04:48 +0530

    ques 14 done

---
+++ DynamicProgram/ques14..py
@@ -0,0 +1,15 @@
+def lcs(self, s1, s2):
+        n=len(s1)
+        m=len(s2)
+        dp= [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s1[j-1]==s2[i-1]:
+                    dp[i][j]= 1+ dp[i-1][j-1]
+                    
+                else:
+                    dp[i][j]= max(dp[i-1][j], dp[i][j-1])
+        
+        return dp[m][n]
