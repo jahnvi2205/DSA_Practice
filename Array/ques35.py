c8d17dae64b61cb147faa75a94ee6e819b428c43
Jahnvi <jahnvi2205@gmail.com>
2026-01-20 02:00:33 +0530

    ques 35 done

--- Array/ques35.py
+++ Array/ques35.py
@@ -1,9 +1,23 @@
-def findMedian(self, arr):
-        #code here.
-        arr.sort()
-        n=len(arr)
-        if n % 2 == 1:
-            return arr[n // 2]
-        else:
-            # Average of the two middle elements
-            return (arr[(n // 2) - 1] + arr[n // 2]) / 2
+def medianOf2(self, a, b):
+        n= len(a)
+        low= 0
+        high= n
+        
+        while low<=high:
+            cutA= (low+high)//2
+            cutB= n- cutA
+            
+            leftA= a[cutA-1] if cutA>0 else float('-inf')
+            rightA= a[cutA] if cutA<n else float('inf')
+            
+            leftB= b[cutB-1] if cutB>0 else float('-inf')
+            rightB= b[cutB] if cutB<n else float('inf')
+            
+            if leftA<=rightB and leftB<=rightA:
+                return (max(leftA,leftB)+min(rightA,rightB))/ 2
+                
+            elif leftA>rightB:
+                high= cutA-1
+                
+            else:
+                low= cutA+1
