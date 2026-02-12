a1401924d5be4fb4fea654209aade1a088028053
Jahnvi <jahnvi2205@gmail.com>
2026-02-13 01:44:49 +0530

    ques 6 done

---
+++ Binary_Tree/Ques6.py
@@ -0,0 +1,27 @@
+def inorder_recurse(node):
+    if not node:
+        return
+    
+    inorder_recurse(node.left)
+    print(node.data, end=" ")
+    inorder_recurse(node.right)
+
+def inorder_iterative(root):
+    stack = []
+    curr = root
+    
+    while curr or stack:
+        
+        # Go to leftmost node
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+        
+        # Process node
+        curr = stack.pop()
+        print(curr.data, end=" ")
+        
+        # Move to right subtree
+        curr = curr.right
+
+
